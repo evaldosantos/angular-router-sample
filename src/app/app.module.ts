19966f075aa9856ea6691b3e48aad60a8be641fb
Evaldo Santos <jesantosjr@protonmail.com>
2021-05-17 19:27:12 -0300

    Heroes feature add heroes functionality hero feature routing requirements remove duplicate hero routes remove heroes declarations module import order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,24 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router'
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
+import { HeroesModule } from './heroes/heroes.module';
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { HeroesListComponent } from './heroes-list/heroes-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AppRoutingModule } from './app-routing.module';
-
-const appRoutes = [
-  { path: 'crisis-list', component: CrisisListComponent },
-  { path: 'heroes-list', component: HeroesListComponent },
-  { path: '', redirectTo: '/heroes-list', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent }
-];
 
 @NgModule({
   declarations: [
     AppComponent,
     CrisisListComponent,
-    HeroesListComponent,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes, { enableTracing: true }),
-    AppRoutingModule
+    FormsModule,
+    HeroesModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
