a0402f7ec9057ea4919cdc525e66553af90b119c
Evaldo Santos <jesantosjr@protonmail.com>
2021-05-17 16:45:49 -0300

    enable route tracing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,13 @@ import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+const appRoutes = [
+  { path: 'crisis-list', component: CrisisListComponent },
+  { path: 'heroes-list', component: HeroesListComponent },
+  { path: '', redirectTo: '/heroes-list', pathMatch: 'full' },
+  { path: '**', redirectTo: '/heroes-list', pathMatch: 'full' }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,12 +23,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([
-      { path: 'crisis-list', component: CrisisListComponent },
-      { path: 'heroes-list', component: HeroesListComponent },
-      { path: '', redirectTo: '/heroes-list', pathMatch: 'full' },
-      { path: '**', redirectTo: '/heroes-list', pathMatch: 'full' }
-    ])
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
