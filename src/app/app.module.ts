bd98859099cd1e520c16323846988d7750ddd788
Evaldo Santos <jesantosjr@protonmail.com>
2021-05-17 15:46:32 -0300

    add heroes-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CrisisListComponent
+    CrisisListComponent,
+    HeroesListComponent
   ],
   imports: [
     BrowserModule
