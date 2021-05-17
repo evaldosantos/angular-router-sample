475b440151ff30d82540c7df7a89daecd3475e1b
Evaldo Santos <jesantosjr@protonmail.com>
2021-05-17 16:27:09 -0300

    adding a 404 page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CrisisListComponent,
-    HeroesListComponent
+    HeroesListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: 'crisis-list', component: CrisisListComponent },
       { path: 'heroes-list', component: HeroesListComponent },
-      { path: '', redirectTo: '/heroes-list', pathMatch: 'full'}
+      { path: '', redirectTo: '/heroes-list', pathMatch: 'full' },
+      { path: '**', redirectTo: '/heroes-list', pathMatch: 'full' }
     ])
   ],
   providers: [],
