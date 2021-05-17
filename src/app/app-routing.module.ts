3bee140851784a773951b0318fac1d4966fb39c8
Evaldo Santos <jesantosjr@protonmail.com>
2021-05-17 17:16:08 -0300

    Refactor the routing configuration into a routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,25 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router'
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AppRoutingModule } from './app-routing.module';
 
-const appRoutes = [
+const appRoutes: Routes = [
   { path: 'crisis-list', component: CrisisListComponent },
   { path: 'heroes-list', component: HeroesListComponent },
   { path: '', redirectTo: '/heroes-list', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
-];
+]
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CrisisListComponent,
-    HeroesListComponent,
-    PageNotFoundComponent
-  ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(appRoutes, { enableTracing: true }),
-    AppRoutingModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  exports: [
+    RouterModule
+  ]
 })
-export class AppModule { }
+export class AppRoutingModule { }
